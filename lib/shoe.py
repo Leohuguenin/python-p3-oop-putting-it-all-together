a609feb9f8cc533da419659da254628b8dad1529
Leohuguenin <leonardohuguenin@live.com>
2025-10-20 16:07:50 +1100

    Implement Book and Shoe classes with properties
    Added constructors, property methods, and behaviors to Book and Shoe classes. Book now supports page_count validation and a turn_page method; Shoe supports size and condition properties with validation and a cobble method.

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    def get_size(self):
+        return self._size 
+
+    def set_size(self, size):
+        if type(size) is int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def get_condition(self):
+        return self._condition
+
+    def set_condition(self, condition):
+        self._condition = condition
+    
+    condition = property(get_condition, set_condition)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
